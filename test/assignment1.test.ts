15eaae77fe3537366b2e7d990cdee1f68da70305
Wu SongYu <wuwilliam2002@gmail.com>
2025-03-21 22:46:55 +0000

    start book-api

---
+++ test/assignment1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Assignment1 from '../lib/assignment1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/assignment1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Assignment1.Assignment1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
